041dbcde2a234d2d42cff64c34f5c2a008ffe8dc
roshnirana-2 <roshnirana0369@gmail.com>
2024-03-16 19:07:31 +0530

    added certificate page

---
+++ certificate.php
@@ -0,0 +1,68 @@
+<?php
+ include "header.php";
+?>
+<!-- header end -->
+<main>
+    <!--? Hero Start -->
+    <div class="slider-area2">
+        <div class="slider-height2 d-flex align-items-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="hero-cap hero-cap2 pt-20 text-center">
+                            <h2>Certificates</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Our Cases Start -->
+    <div class="our-cases-area pb-160">
+        <div class="container">
+            <!-- <div class="row justify-content-center">
+                <div class="col-6">
+                    <img src="assets/img/certificate/cer1.jpg" alt="">
+                </div>
+                <div class="col-6">
+                      <img src="assets/img/certificate/cer3.jpg" alt="">
+                </div>
+                <div class="col-6">
+                    <img src="assets/img/certificate/cer2.png" alt="">
+                </div>
+                <div class="col-6">
+                    <img src="assets/img/certificate/Fcra_original.png" alt="">
+                </div>
+
+                
+            </div> -->
+            <div class="row ">
+                <div class="col-6">
+                    <div class="col-md-12 border border-dark">
+                        <img src="assets/img/certificate/cer1.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="col-md-12 border border-dark">
+                        <img src="assets/img/certificate/cer3.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-6 mt-4">
+                    <div class="col-md-12 border border-dark">
+                        <img src="assets/img/certificate/cer2.png" alt="" class="img-responsive" width = "100%" >
+                    </div>
+                </div>
+                <div class="col-6 mt-4">
+                    <div class="col-md-12 border border-dark">
+                        <img src="assets/img/certificate/Fcra_original.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Our Cases End -->
+</main>
+
+<?php 
+include "footer.php";
+?>
