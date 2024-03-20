e1fd903a42be605d59f10bc542bb50b23a583f0f
Neha <nehasurati1605@gmail.com>
2024-03-20 18:02:53 +0530

    added video in about page

--- about.php
+++ about.php
@@ -2,8 +2,8 @@
 include "header.php";
 
 ?>
-    <main>
-    
+<main>
+
 
     <!--? About Law Start-->
     <section class="about-low-area section-padding2">
@@ -16,20 +16,39 @@ include "header.php";
                             <span>About The Founder</span>
                             <h2>Mr. Ganesh Vasava</h2>
                         </div>
-                        <p class="text-justify font-weight-bold">Sri Sri Adivasi Gram Charitable Trust was formed with the main objective of being helpful for the development of the people of Khewada and the tribal area of Khewada.</p>
-                        <p class="text-justify font-weight-bold ">The trust was approved on the eighth day of the revolutionary month of August 2008. Since then till today, many social useful activities have been done by the trust.</p>
-                        <p class="text-justify font-weight-bold">Mainly the tribal area of Tapi, Narmada and Dang district has been the working area of our trust. In this area many social evils like superstition, addiction are prevalent due to unemployment, poverty and illiteracy. The trust has many projects like tree plantation for public awareness, de-addiction, self-reliance and environmental enrichment. have been carried out.</p>
-                        <p class="text-justify font-weight-bold">So far, more than 1000 youths have been completely freed from addiction through the Addiction Mukti Abhiyan. As many as 400 youths have been made yoga gurus by imparting yoga training. Those who have gained foothold as yoga teachers.
-The trust distributes fruit tree saplings free of charge, from which many families today ripen their own Ambawadi mangoes. Also getting income from other fruit trees. Along with people becoming self-sufficient, the environment is also being enriched through tree cultivation.
+                        <p class="text-justify font-weight-bold">Sri Sri Adivasi Gram Charitable Trust was formed with
+                            the main objective of being helpful for the development of the people of Khewada and the
+                            tribal area of Khewada.</p>
+                        <p class="text-justify font-weight-bold ">The trust was approved on the eighth day of the
+                            revolutionary month of August 2008. Since then till today, many social useful activities
+                            have been done by the trust.</p>
+                        <p class="text-justify font-weight-bold">Mainly the tribal area of Tapi, Narmada and Dang
+                            district has been the working area of our trust. In this area many social evils like
+                            superstition, addiction are prevalent due to unemployment, poverty and illiteracy. The trust
+                            has many projects like tree plantation for public awareness, de-addiction, self-reliance and
+                            environmental enrichment. have been carried out.</p>
+                        <p class="text-justify font-weight-bold">So far, more than 1000 youths have been completely
+                            freed from addiction through the Addiction Mukti Abhiyan. As many as 400 youths have been
+                            made yoga gurus by imparting yoga training. Those who have gained foothold as yoga teachers.
+                            The trust distributes fruit tree saplings free of charge, from which many families today
+                            ripen their own Ambawadi mangoes. Also getting income from other fruit trees. Along with
+                            people becoming self-sufficient, the environment is also being enriched through tree
+                            cultivation.
                         </p>
-                        <p class="text-justify font-weight-bold">Animal donation activity of the trust has also been very useful for self-reliance and self-employment. As many as 440 livestock from Panjrapol have been distributed to the needy people for animal husbandry from which these families have become self-sufficient by producing milk and manure.</p>
-                        <p class="text-justify font-weight-bold">By giving systematic training in organic farming through the trust, many farmers have been made to buy organic produce through organic farming. This experiment of environmental enrichment and land conservation along with economic employment is truly exemplary.</p>
-                        
-                       
+                        <p class="text-justify font-weight-bold">Animal donation activity of the trust has also been
+                            very useful for self-reliance and self-employment. As many as 440 livestock from Panjrapol
+                            have been distributed to the needy people for animal husbandry from which these families
+                            have become self-sufficient by producing milk and manure.</p>
+                        <p class="text-justify font-weight-bold">By giving systematic training in organic farming
+                            through the trust, many farmers have been made to buy organic produce through organic
+                            farming. This experiment of environmental enrichment and land conservation along with
+                            economic employment is truly exemplary.</p>
 
-                    
+
+
+
                     </div>
-                    
+
                 </div>
                 <div class="col-lg-4 col-md-12 ">
                     <!-- about-img -->
@@ -42,19 +61,32 @@ The trust distributes fruit tree saplings free of charge, from which many famili
                             <img src="assets/img/hero/founder .png" alt="" width="354px" height="453px" class="img-fluid ">
                         </div> -->
                         <div class="about-back-img mt-145">
-                            <img src="assets/img/hero/founder.jpeg" alt=""  class="img-fluid ">
+                            <img src="assets/img/hero/founder.jpeg" alt="" class="img-fluid ">
                         </div>
                     </div>
                 </div>
                 <div class="about-caption mb-50">
-                    <div class="col-lg-12 col-md-12"> 
-                    <p class="text-justify font-weight-bold">Blood donation camps are also organized by the trust for health facilities in the area. It is also our determination and effort to build an air-conditioned blood donation center in this area considering the future need.</p>
-                        
-                    <p class="text-justify font-weight-bold">The trust also conducts classes for preparation of competitive examinations to help the youth of the society get employment out of the vicious cycle of unemployment. As a result of which many youths have been able to get government jobs. The Trust provides motivation and encouragement to pursue engineering and doctorate education.</p>
-                    <p class="text-justify font-weight-bold">As a part of social responsibility, the trust also carried out the work of filling Gamegam re-widowshayah forms in Uchchal, Nizar and Kukarmunda talukas. This activity of the trust was also encouraged by the former collector Shri.</p>
+                    <div class="col-lg-12 col-md-12">
+                        <p class="text-justify font-weight-bold">Blood donation camps are also organized by the trust
+                            for health facilities in the area. It is also our determination and effort to build an
+                            air-conditioned blood donation center in this area considering the future need.</p>
 
-                    <p class="text-justify font-weight-bold">Thus Sri Sri Adivasi Gramya Vikas Charitable Trust takes the lead in the work of social upliftment through various activities. We are continuously active in the direction of increasing the scope of such educational, service and social activities in the future also in the field of education, health and cooperation in the direction of the people of our area.</p>
-                    
+                        <p class="text-justify font-weight-bold">The trust also conducts classes for preparation of
+                            competitive examinations to help the youth of the society get employment out of the vicious
+                            cycle of unemployment. As a result of which many youths have been able to get government
+                            jobs. The Trust provides motivation and encouragement to pursue engineering and doctorate
+                            education.</p>
+                        <p class="text-justify font-weight-bold">As a part of social responsibility, the trust also
+                            carried out the work of filling Gamegam re-widowshayah forms in Uchchal, Nizar and
+                            Kukarmunda talukas. This activity of the trust was also encouraged by the former collector
+                            Shri.</p>
+
+                        <p class="text-justify font-weight-bold">Thus Sri Sri Adivasi Gram Vikas Charitable Trust takes
+                            the lead in the work of social upliftment through various activities. We are continuously
+                            active in the direction of increasing the scope of such educational, service and social
+                            activities in the future also in the field of education, health and cooperation in the
+                            direction of the people of our area.</p>
+
                     </div>
                 </div>
             </div>
@@ -62,124 +94,140 @@ The trust distributes fruit tree saplings free of charge, from which many famili
     </section>
     <!-- About Law End-->
 
-     <!-- Our Cases Start -->
-     <div class="our-cases-area section-padding30">
+    <!-- Our Cases Start -->
+    <div class="our-cases-area section-padding30">
         <div class="container">
             <div class="row">
                 <div class="col-lg-6 col-md-6 col-sm-6">
                     <div class="single-cases mb-40">
                         <div class="cases-img">
-                        <video controls height="300" >
-                            <source src="assets/img/video/vid-4.mp4"  type="video/mp4">
-                        </video>
+                            <video controls height="300">
+                                <source src="assets/img/video/vid-4.mp4" type="video/mp4">
+                            </video>
                         </div>
                         <div class="cases-caption">
-                            <h3>Sureshbhai devjibhai mavchi showing gratitude for SHRI SHRI ADIVASI CHARITABLE TRUST</h3>
-                            
+                            <h3>Sureshbhai devjibhai mavchi showing gratitude for SHRI SHRI ADIVASI GRAM
+                                VIKAS CHARITABLE TRUST
+                            </h3>
+
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-6">
                     <div class="single-cases mb-40">
                         <div class="cases-img">
-                        <video controls >
-                            <source src="assets/img/video/vid-3.mp4"  type="video/mp4">
-                        </video>
+                            <video controls>
+                                <source src="assets/img/video/vid-3.mp4" type="video/mp4">
+                            </video>
                         </div>
                         <div class="cases-caption">
-                            <h3>Shakuben gavjibhai gamit sharing her thoughts SHRI SHRI ADIVASI CHARITABLE TRUST</h3>
-                            
+                            <h3>Shakuben gavjibhai gamit sharing her thoughts SHRI SHRI ADIVASI GRAM
+                                VIKAS CHARITABLE TRUST</h3>
+
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-6">
                     <div class="single-cases mb-40">
                         <div class="cases-img">
-                        <video controls height="300" >
-                            <source src="assets/img/video/yoga-vdeo-4.mp4"  type="video/mp4">
-                        </video>
+                            <video controls height="300">
+                                <source src="assets/img/video/yoga-vdeo-4.mp4" type="video/mp4">
+                            </video>
                         </div>
                         <div class="cases-caption">
                             <h3>Kids learning yoga with fun and music.</h3>
-                            
+
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-6">
                     <div class="single-cases mb-40">
                         <div class="cases-img">
-                        <video controls height="300" >
-                            <source src="assets/img/video/yoga-video-1.mp4"  type="video/mp4">
-                        </video>
+                            <video controls height="300">
+                                <source src="assets/img/video/yoga-video-1.mp4" type="video/mp4">
+                            </video>
                         </div>
                         <div class="cases-caption">
                             <h3>Children embracing yoga through playful melodies and joyful movements.</h3>
-                            
+
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-6">
                     <div class="single-cases mb-40">
                         <div class="cases-img">
-                        <video controls height="300" >
-                            <source src="assets/img/video/yoga-video-2.mp4"  type="video/mp4">
-                        </video>
+                            <video controls height="300">
+                                <source src="assets/img/video/yoga-video-2.mp4" type="video/mp4">
+                            </video>
                         </div>
                         <div class="cases-caption">
                             <h3>Kids finding inner peace with yoga, accompanied by lively tunes and laughter.</h3>
-                            
+
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-6">
                     <div class="single-cases mb-40">
                         <div class="cases-img">
-                        <video controls height="300" >
-                            <source src="assets/img/video/yoga-video-3.mp4"  type="video/mp4">
-                        </video>
+                            <video controls height="300">
+                                <source src="assets/img/video/yoga-video-3.mp4" type="video/mp4">
+                            </video>
                         </div>
                         <div class="cases-caption">
                             <h3>Kids learning yoga with fun and music.</h3>
-                            
+
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-6">
                     <div class="single-cases mb-40">
                         <div class="cases-img">
-                        <video controls height="300" >
-                            <source src="assets/img/video/yoga-vdeo-4.mp4"  type="video/mp4">
-                        </video>
+                            <video controls height="300">
+                                <source src="assets/img/video/yoga-vdeo-4.mp4" type="video/mp4">
+                            </video>
                         </div>
                         <div class="cases-caption">
                             <h3>Children embracing yoga through playful melodies and joyful movements.</h3>
-                            
+
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-6">
                     <div class="single-cases mb-40">
                         <div class="cases-img">
-                        <video controls height="300" >
-                        <source src="assets/img/video/vid-5.mp4"  type="video/mp4">
-                        </video>
+                            <video controls height="300">
+                                <source src="assets/img/video/vid-5.mp4" type="video/mp4">
+                            </video>
                         </div>
                         <div class="cases-caption">
                             <h3>Sports day celebration at Playground.</h3>
-                            
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6">
+                    <div class="single-cases mb-40">
+                        <div class="cases-img">
+                            <video controls height="300">
+                                <source src="assets/img/video/vid-6.mp4" type="video/mp4">
+                            </video>
+                        </div>
+                        <div class="cases-caption">
+                            <h3>Blood donors save lives, and today, I'm living proof. Thank you SHRI SHRI ADIVASI GRAM
+                                VIKAS CHARITABLE TRUST for giving me the gift of life.</h3>
+
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-     </div>
+    </div>
 
 
 
-    
 
-    </main>
-    <?php
-    include "footer.php";
-    ?>
-   
+
+</main>
+<?php
+include "footer.php";
+?>
