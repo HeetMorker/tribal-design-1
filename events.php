dfb30e611f5aa0065d0fbee55a2baf2405fc198e
Neha <nehasurati1605@gmail.com>
2024-03-27 18:42:39 +0530

    change play button css

--- events.php
+++ events.php
@@ -28,7 +28,7 @@ include "header.php";
                     <section class="hero is-light is-medium bg-transparent">
                         <div class="hero-foot mb-5 position-relative" style="overflow-x: hidden;">
                             <div class="d-flex justify-content-end">
-                                <i id="leftArrow" class="fa-solid fa-angle-left"style="color:#030E4E"></i>&nbsp;
+                                <i id="leftArrow" class="fa-solid fa-angle-left" style="color:#030E4E"></i>&nbsp;
                                 <i id="rightArrow" class="fa-solid fa-angle-right" style="color:#030E4E"></i>
                             </div>
                             <ul id="buttonGroup" class="d-inline-flex">
@@ -44,11 +44,12 @@ include "header.php";
                                             id="deadd">De-Addiction</a></button></li>
                                 <li class=" "><button class="btn mas-btn"> <a class="" id="organic">Organic
                                             Farming</a></button></li>
-                                <li class=" "><button class="btn mas-btn"> <a class="" id="cow">Cow Donation</a></button></li>
+                                <li class=" "><button class="btn mas-btn"> <a class="" id="cow">Cow
+                                            Donation</a></button></li>
                                 <li class=" "><button class="btn mas-btn"> <a class="" id="tree">Tree Planting
-                                            </a></button></li>
+                                        </a></button></li>
                                 <li class=" "><button class="btn mas-btn"> <a class="" id="fest">Adivasi Festivals
-                                            </a></button></li>
+                                        </a></button></li>
                             </ul>
 
                         </div>
@@ -80,31 +81,48 @@ include "header.php";
                             <div class="grid-item organic"><img src="assets/img/organic farming/fer1.jpg" alt="" />
                             </div>
                             <div class="grid-item yoga"><img src="assets/img/yoga/yoga2.jpg" alt="" /></div>
-                             <div class="grid-item cow"><img src="assets/img/cow/cow1.jpg"  alt=""/></div>
-                            <div class="grid-item cow"><img src="assets/img/cow/cow2.jpg"  alt=""/></div>
-                            <div class="grid-item tree"><img src="assets/img/organic farming/tree1.jpg"  alt=""/></div>
-                            <div class="grid-item tree"><img src="assets/img/organic farming/tree2.jpg" alt=""/></div>    
+                            <div class="grid-item cow"><img src="assets/img/cow/cow1.jpg" alt="" /></div>
+                            <div class="grid-item cow"><img src="assets/img/cow/cow2.jpg" alt="" /></div>
+                            <div class="grid-item tree"><img src="assets/img/organic farming/tree1.jpg" alt="" /></div>
+                            <div class="grid-item tree"><img src="assets/img/organic farming/tree2.jpg" alt="" /></div>
                             <div class="grid-item fest">
-                                <video controls>
-                                    <source src="assets/img/adivasi festival/vid1.mp4" type="video/mp4">
-                                </video>
-                            </div>    
+                                <div class="cases-img">
+                                    <a data-fancybox data-width="940" data-height="660"
+                                        href="assets/img/video/tribal_enjoyment1.mp4"><i
+                                            class="fa-solid fa-play"></i></a>
+                                    <video height="300">
+                                        <source src="assets/img/video/tribal_enjoyment1.mp4" type="video/mp4">
+                                    </video>
+                                </div>
+                            </div>
                             <div class="grid-item fest">
-                                <video controls>
-                                    <source src="assets/img/adivasi festival/vid2.mp4" type="video/mp4">
-                                </video>
-                            </div>   
+                                <div class="cases-img">
+                                    <a data-fancybox data-width="940" data-height="660"
+                                        href="assets/img/adivasi festival/vid1.mp4"><i class="fa-solid fa-play" ></i></a>
+                                    <video height="300">
+                                        <source src="assets/img/adivasi festival/vid2.mp4" type="video/mp4">
+                                    </video>
+                                </div>
+                            </div>
                             <div class="grid-item fest">
-                                <video controls>
-                                    <source src="assets/img/adivasi festival/vid3.mp4" type="video/mp4">
-                                </video>
-                            </div>    
+                                <div class="cases-img">
+                                    <a data-fancybox data-width="940" data-height="660"
+                                        href="assets/img/adivasi festival/vid3.mp4"><i class="fa-solid fa-play"></i></a>
+                                    <video height="300">
+                                        <source src="assets/img/adivasi festival/vid3.mp4" type="video/mp4">
+                                    </video>
+                                </div>
+                            </div>
                             <div class="grid-item fest">
-                                <video controls>
-                                    <source src="assets/img/adivasi festival/vid4.mp4" type="video/mp4">
-                                </video>
-                            </div>    
-                        </div> 
+                                <div class="cases-img">
+                                    <a data-fancybox data-width="940" data-height="660"
+                                        href="assets/img/adivasi festival/vid4.mp4"><i class="fa-solid fa-play"></i></a>
+                                    <video height="300">
+                                        <source src="assets/img/adivasi festival/vid4.mp4" type="video/mp4">
+                                    </video>
+                                </div>
+                            </div>
+                        </div>
                     </section>
                 </div>
 
@@ -119,96 +137,97 @@ include "header.php";
 <script src='https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js'></script>
 <script src='https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js'></script>
 <script>
-    document.addEventListener("DOMContentLoaded", function () {
-        // Initialize Masonry grid
-        var grid = document.querySelector('.grid');
-        var msnry = new Masonry(grid, {
-            itemSelector: '.grid-item',
-            columnWidth: '.grid-sizer',
-            percentPosition: true
-        });
-
-        // Element selectors
-        var imageSets = {
-            'all': document.querySelectorAll('.all'),
-            'blood_donation': document.querySelectorAll('.blood_donation'),
-            'yoga': document.querySelectorAll('.yoga'),
-            'employment': document.querySelectorAll('.employment'),
-            'deadd': document.querySelectorAll('.deadd'),
-            'organic': document.querySelectorAll('.organic'),
-             'cow': document.querySelectorAll('.cow'),
-             'tree': document.querySelectorAll('.tree'),
-             'fest': document.querySelectorAll('.fest')
-        };
-
-        // Buttons
-        const tabsUl = document.getElementById('buttonGroup');
-
-        // Function to toggle active class on buttons
-        function toggleActiveButton(clickedButton) {
-            Array.from(tabsUl.children).forEach(child => {
-                child.classList.remove('is-active');
-            });
-            clickedButton.parentNode.classList.add('is-active');
-        }
-
-        // Function to show images based on button clicked
-        function showImages(imageSetToShow) {
-            if (imageSetToShow === 'all') {
-                // Show all images
-                Object.keys(imageSets).forEach(key => {
-                    const images = imageSets[key];
-                    images.forEach(img => {
-                        img.style.display = 'block';
-                    });
-                });
-            } else {
-                // Show images for specific set
-                Object.keys(imageSets).forEach(key => {
-                    const images = imageSets[key];
-                    const display = (key === imageSetToShow) ? 'block' : 'none';
-                    images.forEach(img => {
-                        img.style.display = display;
-                    });
-                });
-            }
-            msnry.layout(); // Layout Masonry
-        }
-
-        // Event listener for button clicks
-        tabsUl.addEventListener('click', (event) => {
-            if (event.target.tagName === 'BUTTON' || event.target.tagName === 'A') {
-                const button = event.target.tagName === 'BUTTON' ? event.target : event.target.closest('button');
-                const tabId = button.querySelector('a').id;
-                toggleActiveButton(button);
-                showImages(tabId);
-            }
-        });
-
-        // Expand grid item on click
-        grid.addEventListener('click', function (event) {
-            const imgContainer = event.target.parentNode;
-            imgContainer.classList.toggle('grid-item__expanded');
-            msnry.layout();
-        });
+document.addEventListener("DOMContentLoaded", function() {
+    // Initialize Masonry grid
+    var grid = document.querySelector('.grid');
+    var msnry = new Masonry(grid, {
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true
     });
+
+    // Element selectors
+    var imageSets = {
+        'all': document.querySelectorAll('.all'),
+        'blood_donation': document.querySelectorAll('.blood_donation'),
+        'yoga': document.querySelectorAll('.yoga'),
+        'employment': document.querySelectorAll('.employment'),
+        'deadd': document.querySelectorAll('.deadd'),
+        'organic': document.querySelectorAll('.organic'),
+        'cow': document.querySelectorAll('.cow'),
+        'tree': document.querySelectorAll('.tree'),
+        'fest': document.querySelectorAll('.fest')
+    };
+
+    // Buttons
+    const tabsUl = document.getElementById('buttonGroup');
+
+    // Function to toggle active class on buttons
+    function toggleActiveButton(clickedButton) {
+        Array.from(tabsUl.children).forEach(child => {
+            child.classList.remove('is-active');
+        });
+        clickedButton.parentNode.classList.add('is-active');
+    }
+
+    // Function to show images based on button clicked
+    function showImages(imageSetToShow) {
+        if (imageSetToShow === 'all') {
+            // Show all images
+            Object.keys(imageSets).forEach(key => {
+                const images = imageSets[key];
+                images.forEach(img => {
+                    img.style.display = 'block';
+                });
+            });
+        } else {
+            // Show images for specific set
+            Object.keys(imageSets).forEach(key => {
+                const images = imageSets[key];
+                const display = (key === imageSetToShow) ? 'block' : 'none';
+                images.forEach(img => {
+                    img.style.display = display;
+                });
+            });
+        }
+        msnry.layout(); // Layout Masonry
+    }
+
+    // Event listener for button clicks
+    tabsUl.addEventListener('click', (event) => {
+        if (event.target.tagName === 'BUTTON' || event.target.tagName === 'A') {
+            const button = event.target.tagName === 'BUTTON' ? event.target : event.target.closest(
+                'button');
+            const tabId = button.querySelector('a').id;
+            toggleActiveButton(button);
+            showImages(tabId);
+        }
+    });
+
+    // Expand grid item on click
+    grid.addEventListener('click', function(event) {
+        const imgContainer = event.target.parentNode;
+        imgContainer.classList.toggle('grid-item__expanded');
+        msnry.layout();
+    });
+});
 </script>
 <script>
-    var translateX = 0;
-    document.getElementById("rightArrow").addEventListener("click", function () {
-        if (translateX != -600) {
-            translateX = translateX - 200;
-            document.getElementById("buttonGroup").style.transform = "translateX(" + translateX + "px)";
-        }
+var translateX = 0;
+document.getElementById("rightArrow").addEventListener("click", function() {
+    if (translateX != -600) {
+        translateX = translateX - 200;
+        document.getElementById("buttonGroup").style.transform = "translateX(" + translateX + "px)";
+    }
 
-    });
-    document.getElementById("leftArrow").addEventListener("click", function () {
-        if (translateX !=0) {
-            translateX = translateX + 200;
-            document.getElementById("buttonGroup").style.transform = "translateX(" + translateX + "px)";
-        }
+});
+document.getElementById("leftArrow").addEventListener("click", function() {
+    if (translateX != 0) {
+        translateX = translateX + 200;
+        document.getElementById("buttonGroup").style.transform = "translateX(" + translateX + "px)";
+    }
 
-    });
+});
 </script>
 
 
